e6fb1f49b21cc8ee1f840046eeb35bd79ab406c7
zakarialaoui10 <zakarialaoui10@gmail.com>
2023-10-28 18:36:53 +0100

    add Numeric/index

--- dist/index.cjs
+++ dist/index.cjs
@@ -3,6 +3,56 @@
 const { PI: PI$1, E: E$1 } = Math;
 const EPSILON=Number.EPSILON;
 
+var Signal={
+    _map(func){
+
+    },
+    linspace(){
+
+    },
+    logspace(){
+
+    },
+    arange(){
+
+    },
+    echelon(t,t0=0){
+        if(typeof t==="number")return +(t>=t0);
+    },
+    rampe(t,t0=0){
+        if(typeof t==="number")return (t>=t0)?t-t0:0;
+    },
+    sign(t,t0=0){
+        if(typeof t==="number")return Math.sign(t-t0);
+    },
+    rect(t,T,t0=0){
+        if(typeof t==="number")return this.echelon(t,t0-T/2)-(this.echelon(t,t0+T/2));
+    },
+    tri(t,T,t0){
+        if(typeof t==="number"){
+            if(Math.abs(t)>T/2)return 0;
+            else if(t<t0)return this.rampe(t,t0)
+            else return -this.rampe(t,t0)
+        }
+    },
+    dirac(){
+
+    },
+    lorentz(t,t0=0){
+        if(typeof t==="number")return 1/(1+(t-t0)**2);
+    },
+    sinc(){
+
+    },
+    square(){
+
+    },
+    sawtooth(){
+
+    }
+    
+};
+
 const mapFun=(fun,...X)=>{
     const Y=X.map(x=>{
         if(x===null)return fun(null);
@@ -1627,7 +1677,7 @@ const Math$1={
     Combinaison,
     PowerSet,
     subset,*/
-    //Signal,
+    Signal,
     ExtractAll:function(){
         if(window){
             for (let i = 0; i < Object.keys(Ziko.Math).length; i++) {
