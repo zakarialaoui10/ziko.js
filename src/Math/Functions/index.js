2859c3d68fb6fd1628ab96494eddbbffc5abb165
zakarialaoui10 <zakarialaoui10@gmail.com>
2023-10-28 17:29:48 +0100

    add Functions/utils

--- src/Math/Functions/index.js
+++ src/Math/Functions/index.js
@@ -1,47 +1,11 @@
+import { mapFun } from "./utils.js";
 //import ZikoMath from "./index.js"
 //import{Matrix} from "./Matrix.js"
 //import{complex, Complex} from "./Complex.js"
 // var a=complex(1,1)
 // console.log(a instanceof Complex)
 //mapArgs=(fun,...args1)=>(...args2)=>new Array(args1.length).fill(null).map((n,i)=>fun(args1[i],args2[i]))
-const mapFun=(fun,...X)=>{
-    const Y=X.map(x=>{
-        if(x===null)return fun(null);
-        if(["number","string","boolean","bigint","undefined"].includes(typeof x))return fun(x);
-        if(x instanceof Array)return x.map(n=>mapFun(fun,n));
-        if(ArrayBuffer.isView(x))return x.map(n=>fun(n));
-        if(x instanceof Set)return new Set(mapFun(fun,...[...x]));
-        if(x instanceof Map)return new Map([...x].map(n=>[n[0],mapFun(fun,n[1])]));
-        if(x instanceof Matrix){
-            return new Matrix(x.rows,x.cols,mapFun(x.arr.flat(1)))
-        }
-        if(x instanceof ZikoMath.Complex){
-            const [a,b,z,phi]=[x.a,x.b,x.z,x.phi];
-            switch(fun){
-                case Math.log:return complex(ln(z),phi);
-                case Math.exp:return complex(e(a)*cos(b),e(a)*sin(b));
-                case Math.abs:return z;
-                case Math.sqrt:return complex(sqrt(z)*cos(phi/2),sqrt(z)*sin(phi/2));
-                case Math.cos:return complex(cos(a)*cosh(b),-(sin(a)*sinh(b)));
-                case Math.sin:return complex(sin(a)*cosh(b),cos(a)*sinh(b));
-                case Math.tan:{
-                    const DEN=cos(2*a)+cosh(2*b);
-                    return complex(sin(2*a)/DEN,sinh(2*b)/DEN);
-                }
-                case Math.cosh:return complex(cosh(a)*cos(b),sinh(a)*sin(b));
-                case Math.sinh:return complex(sinh(a)*cos(b),cosh(a)*sin(b));
-                case Math.tanh:{
-                    const DEN=cosh(2*a)+cos(2*b);
-                    return complex(sinh(2*a)/DEN,sin(2*b)/DEN)
-                }
-                //default : return fun(x)
-            }
-        }
-        if(x instanceof Object)return Object.fromEntries(Object.entries(x).map(n=>n=[n[0],mapFun(fun,n[1])]))
 
-    });
-   return Y.length==1?Y[0]:Y; 
-}
 function abs(...x){
     return mapFun(Math.abs,...x);
 }
