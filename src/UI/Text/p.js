ce0834eef5b05db55efcd059d50d1933429297fc
zakarialaoui10 <zakarialaoui10@gmail.com>
2023-10-29 00:09:55 +0100

    add p

---
+++ src/UI/Text/p.js
@@ -0,0 +1,34 @@
+import ZikoUIElement from "../ZikoUIElement.js";
+import {Complex} from "../../Math/Complex/index.js"
+import { text } from "./text.js";
+class ZikoUIParagraphe extends ZikoUIElement {
+    constructor(...value) {
+      super();
+      this.element = document.createElement("p");
+      this.addValue(...value);
+      this.style({margin:0,padding:0});
+      this.render();
+    }
+    addValue(...value) {
+      for (let i = 0; i < value.length; i++) {
+        if (typeof value[i] == "string" || typeof value[i] == "number") {
+          this.element.appendChild(document.createTextNode(value[i]));
+          this.element.appendChild(document.createElement("br"));
+        } else if (value[i] instanceof ZikoUIElement)
+          this.element.appendChild(value[i].element);
+        else if (value[i] instanceof Complex)
+          text(value.a + " + " + value.b + "i");
+      }
+      return this;
+    }
+    clear() {
+      this.element.childNodes.forEach((e) => e.remove());
+      return this;
+    }
+    setValue(...value) {
+      this.clear();
+      this.addValue(...value);
+      return this;
+    }
+  }
+  const p = (...ZikoUIElement) => new ZikoUIParagraphe(...ZikoUIElement);
