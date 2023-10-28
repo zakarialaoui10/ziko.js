9fbcb8332bc2da5bb77db019d636f4266b8d3f6d
zakarialaoui10 <zakarialaoui10@gmail.com>
2023-10-28 23:46:32 +0100

    remove content

--- docs/dir2tree.js
+++ docs/dir2tree.js
@@ -4,7 +4,7 @@ import dir2tree from "dir2tree"
 const ROOT = path.join(process.cwd(),'..',"src");
 const TARGET = path.join(process.cwd(),".");
 const MyTree = dir2tree(ROOT,{
-  fileContent:true,
+  fileContent:false,
   lineCount:true,
   sortBy:"extension",
   skipFile:["ger.md"],
