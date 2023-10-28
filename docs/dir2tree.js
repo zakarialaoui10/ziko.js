913b5faf19ce7bdf733407c426b6e0dc0998ff9f
zakarialaoui10 <zakarialaoui10@gmail.com>
2023-10-28 23:45:32 +0100

    add dir2tree to docs

--- docs/dir2tree.js
+++ docs/dir2tree.js
@@ -1,15 +1,15 @@
 import fs from "fs"
 import path from "path" 
 import dir2tree from "dir2tree"
-const ROOT = path.join(process.cwd(),'.',"src");
+const ROOT = path.join(process.cwd(),'..',"src");
 const TARGET = path.join(process.cwd(),".");
 const MyTree = dir2tree(ROOT,{
-  fileContent:false,
+  fileContent:true,
+  lineCount:true,
   sortBy:"extension",
   skipFile:["ger.md"],
   skipFolder:[".git","node_modules","assets",""],
   skipExtension:["json"],
 },[]);
-
 console.log(MyTree.tree)
-//MyTree.write(TARGET,"generated.json")
+MyTree.flat(Infinity).write(TARGET,"generated.json")
