242b86b5b7deeed22172c211784c57101236b0a9
zakarialaoui10 <zakarialaoui10@gmail.com>
2023-10-28 19:19:08 +0100

    add removeAll

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ const Ziko={
 Ziko.Math.ExtractAll=function(){
     for (let i = 0; i < Object.keys(Ziko.Math).length; i++) {
         globalThis[Object.keys(Ziko.Math)[i]] = Object.values(Ziko.Math)[i];
+    }
+    return this;
 }
-return this;
+Ziko.RemoveAll=function(){
+    for (let i = 0; i < Object.keys(Ziko.Math).length; i++) delete globalThis[Object.keys(Ziko.Math)[i]];   
+    return this;
 }
 export default Ziko
