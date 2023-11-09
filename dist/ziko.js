e47cc5f4b2af94f31dcbbe73a5da9643eb290d14
zakarialaoui10 <zakarialaoui10@gmail.com>
2023-11-09 12:30:05 +0100

    fix loop

--- dist/ziko.js
+++ dist/ziko.js
@@ -4190,57 +4190,86 @@
     };
 
     class Loop {
-      constructor(callback, fps) {
+      constructor(callback, {fps,step,t=[0,null],start=true}={}) {
         this.callback = callback;
         this.cache = {
           isRunning: false,
-          animationId : null
+          animationId : null,
+          startTime : null,
+          step,
+          fps,
+          t,
+          started:start
         };
-        this.fps = fps;
-        this.startTime = null;
+        this.adjust();
         this.i=0;
       }
-
-      get time_step() {
-        return 1000 / this.fps;
+      adjust(){
+        if(this.cache.step && this.cache.fps){
+          console.warn(`Fps will be adjusted from ${this.cache.fps} to ${1000/this.cache.step} to ensure a smoother animation`);
+          this.cache.fps=1000/this.cache.step;
+        }
+        if(this.cache.started){
+          const t=this.cache.t;
+          t[0]?this.startAfter(t[0]):this.start();
+          if(t[1])this.stopAfter(t[1]);
+        }
+        return this;
       }
-
+      get TIME_STEP() {
+        return this.cache.step?this.cache.step:1000 / this.cache.fps;
+      }
       start() {
         if (!this.cache.isRunning) {
+          this.i=0;
           this.cache.isRunning = true;
-          this.startTime = Date.now();
+          this.cache.startTime = Date.now();
           this.animate();
         }
         return this;
       }
-
-      stop() {
+      pause() {
         if (this.cache.isRunning) {
           clearTimeout(this.cache.animationId);
           this.cache.isRunning = false;
         }
         return this;
       }
-
+      stop(){
+        this.pause();
+        this.i=0;
+        return this;
+      }
+      resume(){
+        this.cache.isRunning=true;
+        this.animate();
+        return this;
+      }
+      startAfter(t=1000){
+        setTimeout(this.start.bind(this),t);
+        return this;
+      }
+      stopAfter(t=1000){
+        setTimeout(this.stop.bind(this),t);
+        return this;   
+      }
       animate = () => {
-        if (!this.cache.isRunning) {
-          return;
-        }
-
-        const now = Date.now();
-        const delta = now - this.startTime;
-
-        if (delta > this.time_step) {
-          this.callback(this);
-          this.i++;
-          this.startTime = now - (delta % this.time_step);
-        }
-
-        this.cache.animationId = setTimeout(this.animate, 0);
-      };
+        if (this.cache.isRunning) {
+          const now = Date.now();
+          const delta = now - this.cache.startTime;
+          if (delta > this.TIME_STEP) {
+            this.callback(this);
+            this.i++;
+            this.cache.startTime = now - (delta % this.TIME_STEP);
+          }
+          this.cache.animationId = setTimeout(this.animate, 0);
+        }  }
     }
 
     const loop = (callback, fps) => new Loop(callback, fps);
+
+
+      loop((e) => console.log(e.i), {step:100,fps:30,t:[500,4001],start:true});
 
     const Ease={
         Linear:function(t){
