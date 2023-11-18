bd64d09e304ff0e1331139fdaccfce81adf71e1e
zakarialaoui10 <zakarialaoui10@gmail.com>
2023-11-18 12:50:13 +0100

    fix tri

--- dist/ziko.mjs
+++ dist/ziko.mjs
@@ -2167,25 +2167,26 @@ var Signal={
 
     },
     echelon(t,t0 = 0){
+        if(!(t instanceof Array))t=[t];
         return mapfun(n=>n>=t0?1:0,...t);
     },
     rampe(t,t0 = 0){
+        if(!(t instanceof Array))t=[t];
         return mapfun(n=>n>=t0?n-t0:0,...t);
     },
     sign(t,t0 = 0){
         if(typeof t==="number")return Math.sign(t-t0);
     },
     rect(t,T,t0 = 0){
+        if(!(t instanceof Array))t=[t];
         return mapfun(n=>((t0-T/2 < n) && (t0+T/2 > n)? 1 : 0),...t)
     },
-    tri(t,T,t0){
-        if(typeof t==="number"){
-            if(Math.abs(t)>T/2)return 0;
-            else if(t<t0)return this.rampe(t,t0)
-            else return -this.rampe(t,t0)
-        }
+    tri(t,T,t0 = 0 , A = 1){
+        if(!(t instanceof Array))t=[t]; 
+        const Y = mapfun(n=>((t0-T/2 < n) && (t0+T/2 > n)? 1 - 2 * abs$1(n/T) : 0),...t);
+        return Y instanceof Array ? Y.map(n=>n*A) : Y*A
     },
-    dirac(t0,...t){
+    dirac(t,t0){
         return mapfun(n=>n===t0?Infinity:0,...t);
     },
     lorentz(t,t0=0){
--- src/Math/Signal/index.js
+++ src/Math/Signal/index.js
@@ -1,4 +1,5 @@
 import { mapfun } from "../Utils";
+import { abs } from "../Functions/index.js"
 
 var Signal={
     linspace(){
@@ -11,25 +12,27 @@ var Signal={
 
     },
     echelon(t,t0 = 0){
+        if(!(t instanceof Array))t=[t]
         return mapfun(n=>n>=t0?1:0,...t);
     },
     rampe(t,t0 = 0){
+        if(!(t instanceof Array))t=[t]
         return mapfun(n=>n>=t0?n-t0:0,...t);
     },
     sign(t,t0 = 0){
         if(typeof t==="number")return Math.sign(t-t0);
     },
     rect(t,T,t0 = 0){
-        return mapfun(n=>((t0-T/2 < n) && (t0+T/2 > n)? 1 : 0),...t)
+        if(!(t instanceof Array))t=[t];
+        const Y = mapfun(n=>((t0-T/2 < n) && (t0+T/2 > n)? 1 - 2 * abs(n/T) : 0),...t)
+        return Y instanceof Array ? Y.map(n=>n*A) : Y*A
     },
-    tri(t,T,t0){
-        if(typeof t==="number"){
-            if(Math.abs(t)>T/2)return 0;
-            else if(t<t0)return this.rampe(t,t0)
-            else return -this.rampe(t,t0)
-        }
+    tri(t,T,t0 = 0 , A = 1){
+        if(!(t instanceof Array))t=[t] 
+        const Y = mapfun(n=>((t0-T/2 < n) && (t0+T/2 > n)? 1 - 2 * abs(n/T) : 0),...t)
+        return Y instanceof Array ? Y.map(n=>n*A) : Y*A
     },
-    dirac(t0,...t){
+    dirac(t,t0){
         return mapfun(n=>n===t0?Infinity:0,...t);
     },
     lorentz(t,t0=0){
