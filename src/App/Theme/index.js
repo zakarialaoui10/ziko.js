05a7eaabfae56e47c93ffcbd2b5cbfc767f8a703
zakarialaoui10 <zakarialaoui10@gmail.com>
2023-12-15 12:39:47 +0100

    add themes

--- src/App/Theme/index.js
+++ src/App/Theme/index.js
@@ -1,5 +1,15 @@
-import { Colors } from "./colors";
+import LightThemes from "./light";
+import DarkThemes from "./dark";
 const Themes={
-    Colors
+    ...LightThemes,
+    ...DarkThemes
 }
-export {Themes};
+
+const keys=Object.keys(Themes);
+const useThemeByName=(app,name)=>{for (a in Themes[name]) app.root.style.setProperty(`--${a}-${app.id}`, Themes[name][a])};
+const useThemeByIndex=(app,index)=>{for (a in Themes[keys[index]]) app.root.style.setProperty(`--${a}-${app.id}`, Themes[keys[index]][a])};
+  export {
+    Themes,
+    useThemeByName,
+    useThemeByIndex
+};
--- src/App/app.js
+++ src/App/app.js
@@ -1,8 +1,10 @@
 import ZikoUIElement from "../UI/ZikoUIElement";
 import { Seo } from "./Seo/index.js";
+import { Themes } from "./Theme/index.js";
 class ZikoUIApp extends ZikoUIElement{
     constructor(){
         super();
+        this.id="ziko-app";
         this.root=document.documentElement;
         this.element=document.createElement("main");
         this.head=null;
@@ -30,11 +32,29 @@ class ZikoUIApp extends ZikoUIElement{
     description(){
 
     }
-    get currentTheme(){
-
+    get Theme(){
+        return{
+            background: `var(--background-${this.id})`,
+            currentLine: `var(--currentLine-${this.id})`,
+            selection: `var(--selection-${this.id})`,
+            foreground: `var(--foreground-${this.id})`,
+            comment: `var(--comment-${this.id})`,
+            cyan: `var(--cyan-${this.id})`,
+            green: `var(--green-${this.id})`,
+            orange: `var(--orange-${this.id})`,
+            pink: `var(--pink-${this.id})`,
+            purple: `var(--purple-${this.id})`,
+            red: `var(--red-${this.id})`,
+            yellow: `var(--yellow-${this.id})`,
+        }
     }
-    useTheme(){
-
+    useTheme(index){
+        const keys=Object.keys(Themes);
+        for(let a in Themes[keys[index]]){
+            this.root.style.setProperty(`--${a}-${this.id}`, Themes[keys[index]][a]);
+        }
+        console.log(keys[index])
+        return this;
     }
 }
 const App=()=>new ZikoUIApp()
