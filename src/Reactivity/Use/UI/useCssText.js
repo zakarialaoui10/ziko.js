58d45b8f907e311b77259f169f67da316a080f27
zakarialaoui10 <zakarialaoui10@gmail.com>
2024-02-11 16:34:51 +0100

    fix all

---
+++ src/Reactivity/Use/UI/useCssText.js
@@ -0,0 +1,21 @@
+class ZikoUseCssText{
+    constructor(css){
+        this.#init();
+        this.set(css);
+    }
+    #init(){     
+        // this.__FavIcon__=document.querySelector("link[rel*='icon']") || document.createElement('link');   
+        // this.__FavIcon__.type = 'image/x-icon';
+        // this.__FavIcon__.rel = 'shortcut icon';
+        return this;
+    }
+    set(cssText){
+        // if(href!==this.__FavIcon__.href){
+        //     this.__FavIcon__.href=href;
+        //     if(this.cache.Emitter)this.cache.Emitter.emit("ziko:favicon-changed");
+        // }
+        return this;
+    }
+}
+const useFavIcon=(FavIcon,useEventEmitter)=>new ZikoUseFavIcon(FavIcon,useEventEmitter);
+export{ useFavIcon }
