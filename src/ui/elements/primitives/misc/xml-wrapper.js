bbd89123dce4d0d0790b8cd4ef97ed2ffdb6971c
zakarialaoui10 <zakarialaoui10@gmail.com>
2024-10-13 15:43:16 +0100

    upgrade

--- src/ui/elements/primitives/misc/xml-wrapper.js
+++ src/ui/elements/primitives/misc/xml-wrapper.js
@@ -7,7 +7,7 @@ class ZikoUIXMLWrapper extends ZikoUIElement{
 }
 function html2dom(htmlString) {
     const parser = new DOMParser();
-    const doc = parser.parseFromString(htmlString, 'text/html');
+    const doc = parser.parseFromString(`<div>${htmlString}</div>`, 'text/html');
     return doc.body.firstChild;
   }
 function svg2dom(svgString) {
