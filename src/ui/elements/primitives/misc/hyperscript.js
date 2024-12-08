cff8edca3226171bffb1540b3755ead615afd1a6
zakarialaoui10 <zakarialaoui10@gmail.com>
2024-12-08 12:20:16 +0100

    upgrade

--- src/ui/elements/primitives/misc/hyperscript.js
+++ src/ui/elements/primitives/misc/hyperscript.js
@@ -1,9 +1,10 @@
 import ZikoUIElement from "../ZikoUIElement.js";
-function h(tag, attributes, ...children){
+function h(tag, attributes = {}, ...children){
     const {name, style, ...attrs} = attributes
     let element = new ZikoUIElement(tag,name);
     style && element.style(style);
-    attrs && element.setAttr(attrs)
+    attrs && element.setAttr(attrs);
+    children && element.append(...children)
     return element
 }
 
