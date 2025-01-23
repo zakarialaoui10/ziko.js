88618cddb4c2ff292a9ab0cad266f108e76eab0e
zakarialaoui10 <zakarialaoui10@gmail.com>
2025-01-23 23:51:09 +0100

    remove custom ui elements

--- src/ui/elements/derived/index.js
+++ src/ui/elements/derived/index.js
@@ -1,12 +1,12 @@
 export * from "./flex";
 export * from "./grid";
-export * from "./accordion";
-export * from "./carousel";
-export * from "./code-note";
-export * from "./tabs";
-export * from "./alert";
-export * from "./splitter";
-export * from "./pagination";
-export * from "./menu";
-export * from "./modal"
-export * from "./slider"
+// export * from "./accordion";
+// export * from "./carousel";
+// export * from "./code-note";
+// export * from "./tabs";
+// export * from "./alert";
+// export * from "./splitter";
+// export * from "./pagination";
+// export * from "./menu";
+// export * from "./modal"
+// export * from "./slider"
